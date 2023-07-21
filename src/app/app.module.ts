71c2aa0d88bae6e7701a067b6f5a11893bdef8ff
Your Name git config --global user.email <mohamedtam914@gmail.com>
2023-07-21 11:35:23 +0200

    update todoCards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatCardModule} from '@angular/material/card';
 
 
 import { FormsModule } from '@angular/forms';
@@ -26,6 +26,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
+    MatCardModule,
     MatIconModule,
     MatDividerModule,
     MatButtonModule,
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject } from '@angular/core';
-import { Firestore, addDoc, collection, collectionData } from '@angular/fire/firestore';
+import { Firestore, addDoc, collection, collectionData, deleteDoc, doc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
-
 
 
 
@@ -13,9 +12,13 @@ import { Observable } from 'rxjs';
   
 })
 export class FormComponent {
-  firestore: Firestore = inject(Firestore)
+  firestore: Firestore = inject(Firestore);
+  todos$ !: Observable<any[]>;
+  todoCollection = collection(this.firestore, 'todos');
 
-  constructor() {}
+  constructor() {
+    this.getTodos()
+  }
     todo = {
       title: '',
       description: ''
@@ -25,6 +28,18 @@ export class FormComponent {
   submitForm() {
     const todoCollection = collection(this.firestore, 'todos')
     addDoc(todoCollection, this.todo )
+  }
+
+  getTodos() {
+    this.todos$ = collectionData(this.todoCollection, {idField : 'id'})
+    this.todos$.subscribe ((todo) => {
+      console.log(todo)
+    })
+  }
+
+  deleteTodo(id: string) {
+    const todoDoc = doc(this.todoCollection, id)
+    deleteDoc(todoDoc)
   }
 
 
